1a25bc41b40c21c321969f3fc3eee8da40769128
MeiXue1014 <chongmx-wm19@student.tarc.edu.my>
2020-07-01 01:39:13 +0800

    homework

---
+++ app/src/main/java/com/example/practical_2_part_b/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.practical_2_part_b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+    private TextView btnZero;
+    private TextView btnCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+        btnZero = (TextView) findViewById(R.id.button_zero);
+        btnCount = (TextView) findViewById(R.id.button_count);
+    }
+
+    public void showToast(View view){
+        Toast toast = Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view){
+        ++mCount;
+        if(mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+            btnZero.setBackgroundColor(Color.RED);
+        }
+        if((mCount%2)==0){
+            btnCount.setBackgroundColor(Color.GREEN);
+        }
+        else{
+            btnCount.setBackgroundColor(Color.BLUE);
+        }
+    }
+
+    public void showZeroCount(View view){
+        btnZero.setBackgroundColor(Color.GRAY);
+        mCount = 0;
+        mShowCount.setText(Integer.toString(mCount));
+    }
+
+}
